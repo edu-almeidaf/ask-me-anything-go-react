4e60db4a153aeead83a455dafff10eb775f8bef7
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-08-09 12:32:15 -0300

    feat(frontend): add share room url option

--- frontend/src/pages/room.tsx
+++ frontend/src/pages/room.tsx
@@ -1,6 +1,7 @@
 import { Share2 } from 'lucide-react'
 import { Suspense } from 'react'
 import { useParams } from 'react-router-dom'
+import { toast } from 'sonner'
 
 import AmaLogo from '@/assets/ama-logo.svg'
 import { CreateMessageForm } from '@/components/create-message-form'
@@ -8,6 +9,17 @@ import { Messages } from '@/components/messages'
 
 export function Room() {
   const { roomId } = useParams()
+
+  function handleShareRoom() {
+    const url = window.location.href.toString()
+
+    if (navigator.share !== undefined && navigator.canShare()) {
+      navigator.share({ url })
+    } else {
+      navigator.clipboard.writeText(url)
+      toast.info('O link da sala foi copiado para a área de transferência!')
+    }
+  }
 
   return (
     <div className="mx-auto flex max-w-[640px] flex-col gap-5 px-4 py-20">
@@ -18,7 +30,11 @@ export function Room() {
           Código da sala: <span className="text-zinc-300">{roomId}</span>
         </span>
 
-        <button className="ml-auto flex items-center gap-1.5 rounded-lg bg-zinc-800 px-3 py-1.5 text-sm font-medium transition-colors hover:bg-zinc-700">
+        <button
+          type="submit"
+          onClick={handleShareRoom}
+          className="ml-auto flex items-center gap-1.5 rounded-lg bg-zinc-800 px-3 py-1.5 text-sm font-medium transition-colors hover:bg-zinc-700"
+        >
           Compartilhar
           <Share2 className="size-4" />
         </button>
